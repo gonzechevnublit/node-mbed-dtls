76490d234817f75c2742415999394dc8901a8457
PeterEB <peter.eb9@gmail.com>
2019-05-02 11:21:53 +0800

    Update downloadDeps.sh script to install mbedtls v2.14.0

--- downloadDeps.sh
+++ downloadDeps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf mbedtls
-git clone https://github.com/ARMmbed/mbedtls.git mbedtls
+git clone --single-branch --branch mbedtls-2.14 https://github.com/ARMmbed/mbedtls.git mbedtls
 
